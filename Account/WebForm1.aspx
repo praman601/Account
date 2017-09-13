14dbf1fae17a178ca466a6f9dd75d23387068da8
pradeep budhathoki <pradeep.budhathoki1234@gmail.com>
2017-09-13 19:41:05 +0545

    another commit

---
+++ Account/WebForm1.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Account.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+
+                <tr>
+                    <td>
+                        <asp:Label ID="Label8" runat="server" Text="Account number"></asp:Label>
+                    </td>
+                    <td>
+
+                        <asp:Label ID="Label1" runat="server" Text="Label1"></asp:Label>
+                        </td>
+                    <td>
+                        <asp:Label ID="Label2" runat="server" Text="Label2"></asp:Label>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label9" runat="server" Text="CustomerName"></asp:Label>
+                    </td>
+                    <td>
+
+                        <asp:Label ID="Label3" runat="server" Text="Label3"></asp:Label>
+                        </td>
+                    <td>
+
+                        <asp:Label ID="Label4" runat="server" Text="Label4"></asp:Label>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label10" runat="server" Text="Balance"></asp:Label>
+
+                    </td>
+                    <td>
+                        <asp:Label ID="Label5" runat="server" Text="Label5"></asp:Label>
+                        </td>
+
+
+                        <td>
+                            <asp:Label ID="Label6" runat="server" Text="Label6"></asp:Label>
+                    </td>
+
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <asp:Button ID="Button1" runat="server" Text="Transfer 10 from A1 to A2" OnClick="Button1_Click" />
+                    </td>
+                </tr>
+
+            </table>
+            <asp:Label ID="Label7" runat="server" Text=""></asp:Label>
+ </div>
+    </form>
+</body>
+</html>
---
+++ Account/WebForm1.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Account
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetData();
+            }
+
+        }
+
+        private void GetData()
+        {
+            using (SqlConnection con = new SqlConnection("Data Source=PRADEEP\\SQLEXPRESS;Initial Catalog=Account;Integrated Security=True;"))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from Accounts", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    if (rdr["AccountNumber"].ToString() == "A1")
+                    {
+                        Label1.Text = "A1";
+                        Label5.Text = rdr["Balance"].ToString();
+                        Label3.Text = rdr["CustomerName"].ToString();
+
+                    }
+                    else
+                    {
+                        Label2.Text = "A2";
+                        Label6.Text = rdr["Balance"].ToString();
+                        Label4.Text = rdr["CustomerName"].ToString();
+                    }
+                }
+
+
+
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection("Data Source=PRADEEP\\SQLEXPRESS;Initial Catalog=Account;Integrated Security=True;"))
+            {
+                con.Open();
+                SqlTransaction transaction = con.BeginTransaction();
+                try
+                {
+                    
+                    SqlCommand cmd = new SqlCommand("update Accounts set Balance=Balance-10 where AccountNumber='A1'", con,transaction);
+                 
+                    cmd.ExecuteNonQuery();
+                    cmd = new SqlCommand("update Accounts set Balance=Balance+10 where AccountNumber='A2'", con,transaction);
+                    cmd.ExecuteNonQuery();
+                    transaction.Commit();
+                    Label7.Text = "Transaction Succesful";
+                    Label7.ForeColor = System.Drawing.Color.Green;
+
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    Label7.Text = "Transaction failed";
+                    Label7.ForeColor = System.Drawing.Color.Red;
+
+                }
+                GetData();
+            }
+        }
+    }
+}
